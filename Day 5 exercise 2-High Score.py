e3a02149d93aa3d733ac7fc59d3ffd75d616b63e
Jorge96273 <115398843+Jorge96273@users.noreply.github.com>
2022-10-16 10:09:39 +0900

    Add files via upload

---
+++ Day 5 exercise 2-High Score.py
@@ -0,0 +1,15 @@
+
+# 🚨 Don't change the code below 👇
+student_scores = input("Input a list of student scores ").split()
+for n in range(0, len(student_scores)):
+  student_scores[n] = int(student_scores[n])
+print(student_scores)
+# 🚨 Don't change the code above 👆
+# 78 65 89 86 55 91 64 89
+#Write your code below this row 👇
+highest_score = 0
+for score in student_scores:
+  if highest_score < score:
+    highest_score = score
+    
+print(f"The highest score in the class is: {highest_score}")
